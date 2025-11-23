4f56766ae1cde56ee745a92ca383ad0111825b61
Vishrut Reddi <vishrutreddi@gmail.com>
2025-11-22 22:37:50 -0800

    feat(web): add tanstack start application

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,82 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+import ClerkProvider from '../integrations/clerk/provider'
+
+import ConvexProvider from '../integrations/convex/provider'
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+
+import StoreDevtools from '../lib/demo-store-devtools'
+
+import appCss from '../styles.css?url'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ClerkProvider>
+          <ConvexProvider>
+            <Header />
+            {children}
+            <TanStackDevtools
+              config={{
+                position: 'bottom-right',
+              }}
+              plugins={[
+                {
+                  name: 'Tanstack Router',
+                  render: <TanStackRouterDevtoolsPanel />,
+                },
+                TanStackQueryDevtools,
+                StoreDevtools,
+              ]}
+            />
+          </ConvexProvider>
+        </ClerkProvider>
+        <Scripts />
+      </body>
+    </html>
+  )
+}
